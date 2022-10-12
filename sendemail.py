31eadb22d966b7bbb1273ebbd98a18de8ecb01df
WTDER <67782037+WTDER@users.noreply.github.com>
2022-10-13 00:23:24 +0800

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -16,14 +16,14 @@ class Email(object):
     def _construct_message(self,is_success:bool,content:str):
         self.message = MIMEText(content, 'plain', 'utf-8')
         self.message['From'] = Header("furuoo", 'utf-8')
-        self.message['To'] =  Header("blli22", 'utf-8')
+        self.message['To'] =  Header("zmshen22", 'utf-8')
         dt = datetime.datetime.today().strftime('%m月%d日 ')
         if is_success:
             self.message['Subject'] = Header(dt+'平安复旦打卡成功提醒 ... ', 'utf-8')
         else:
             self.message['Subject'] = Header(dt+'!!! 失败提醒 ... ', 'utf-8')
 
-    def sendemail(self,is_success:bool,content:str,receivers=['22210240022@m.fudan.edu.cn']):
+    def sendemail(self,is_success:bool,content:str,receivers=['22210240045@m.fudan.edu.cn']):
         self._construct_message(is_success,content)
         try:
             self.smtpObj.connect(self.email_host,25)    # 25 为 SMTP 端口号
